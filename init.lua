4bbee934fe9c16ce9d6989698d282e683c3fa614
Skibidi50-lol <realhatroi@gmail.com>
2026-07-11 20:24:55 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,7 @@
---local version = "Solara" or "B" 
+local version = getgenv().version or "Solara"
 
 if version == "Solara" then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Skibidi50-lol/Ravent/refs/heads/main/version/Solara.lua"))()
-end
-
-if version == "B" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Skibidi50-lol/Ravent/refs/heads/main/version/RaventB.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Skibidi50-lol/Ravent/main/version/Solara.lua"))()
+elseif version == "B" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Skibidi50-lol/Ravent/main/version/RaventB.lua"))()
 end
