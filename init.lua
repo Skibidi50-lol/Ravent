1b03f58c9c3021157434b30ee336eb5f350abe8e
Skibidi50-lol <realhatroi@gmail.com>
2026-07-11 20:16:28 +0700

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,9 @@
+--local version = "Solara" or "B" 
+
+if version == "Solara" then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Skibidi50-lol/Ravent/refs/heads/main/version/Solara.lua"))()
+end
+
+if version == "B" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Skibidi50-lol/Ravent/refs/heads/main/version/RaventB.lua"))()
+end
